fb3ddbaba269b096227ecd413db10c071e501937
Lars Henrik Bolstad <lahebo@gmail.com>
2016-08-25 00:45:19 +0100

    started assignment B

---
+++ ex01/main.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void allocate(int value) {
+    int *ptr = NULL;
+    ptr = malloc(100000 * sizeof(int));
+    *ptr = value;
+    printf("test of allocated memory: %i\n");
+}
+
+int main2() {
+
+    // A
+    //while(1) allocate(3);
+
+    return 0;
+}
