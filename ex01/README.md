445c5e26146bdaf4de778fbe6cd43ef71266ad76
Lars Henrik Bolstad <lahebo@gmail.com>
2016-08-25 16:58:40 +0000

    finished assignment C, restructured

---
+++ ex01/README.md
@@ -0,0 +1,10 @@
+# Exercise 1: Memory, Errors and Time
+
+### Assignment A:
+The `allocate()` function leaks memory.
+
+### Assignment B:
+See [linked_list](/linked_list).
+
+### Assignment C:
+See [allocate.c](allocate.c).
--- ex01/allocate.c
+++ ex01/allocate.c
@@ -4,13 +4,18 @@
 void allocate(int value) {
     int *ptr = NULL;
     ptr = malloc(100000 * sizeof(int));
+
+    // Assignment C:
+    if (ptr == NULL) {
+        perror("Error in allocate: ");
+        return;
+    }
+
     *ptr = value;
     printf("test of allocated memory: %i\n");
 }
 
 int main() {
-
     while(1) allocate(3);
-
     return 0;
 }
